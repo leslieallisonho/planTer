be68fec0a20d0384ca6a061762adf967d707fda4
Wenyuan Bao <wenyuanbao@gmail.com>
2019-07-20 16:50:15 -0700

    Set up app

---
+++ Planter/Planter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planter
+//
+//  Created by Wenyuan Bao on 7/20/19.
+//  Copyright © 2019 PlanTer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
