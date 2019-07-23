4e430954abfe9f8f8fcf586e1629c18f45852c8f
Leslie Ho <leslieallisonho@gmail.com>
2019-07-22 18:00:47 -0700

    adding in custom color file

---
+++ PlanTer/Extensions/UIColor + Planter.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor + Planter.swift
+//  Planter
+//
+//  Created by Leslie Ho on 7/22/19.
+//  Copyright © 2019 PlanTer. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let pltrMainBlue = UIColor(red: 0.45, green: 0.76, blue: 0.87, alpha: 1.0)
+
+    static let pltrLightBlue = UIColor(red: 0.79, green: 0.89, blue: 0.91, alpha: 1.0)
+
+    static let pltrMainGold = UIColor(red: 0.98, green: 0.82, blue: 0.45, alpha: 1.0)
+
+    static let pltrMainGray = UIColor(red: 0.77, green: 0.77, blue: 0.77, alpha: 1.0)
+
+    static let pltrTitleFontGray = UIColor(red: 0.51, green: 0.51, blue: 0.51, alpha: 1.0)
+
+    static let pltrLightBackgroundGray = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.0)
+}
